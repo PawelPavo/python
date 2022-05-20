a7075540d0e701f8a187ae8c0f44106d7a767ca1
PawelPavo <59782585+PawelPavo@users.noreply.github.com>
2022-05-20 16:00:52 -0400

    Add files via upload

--- func_practice_1_LEVEL_1.py
+++ func_practice_1_LEVEL_1.py
@@ -6,47 +6,62 @@
 
 def myfunc(a,b):
     if a%2==0 and b%2==0:
-        mylist=[a,b]
-        return min(mylist)
+        return min(a,b)
     else:
-        mylist=[a,b]
-        return max(mylist)
+        return max(a,b)
 
 
 print(myfunc(2,4))
 print(myfunc(2,5))
+
+#SOLUTION without min function
+def myfunc_solution(a,b):
+    if a%2==0 and b%2==0:
+        if a<b:
+            result = a
+        else:
+            result = b
+    else:
+        if a > b:
+            result = a
+        else:
+            result = b
+    return result
 
 #ANIMAL CRACKERS:
 #Write a function takes a two-word string
 #and returns True if both words begin with same letter
 
 def myfunc2(text):
-    mylist=(text.split(' '))
+    mylist=text.split()
     if mylist[0][0] == mylist[1][0]:
         return True
     else:
         return False
-
-
 print(myfunc2('Levelheaded Llama'))
 print(myfunc2('Crazy Kangaroo'))
+
+
+#SOLUTION
+def myfunc2_solution(text):
+    mylist=text.lower().split()
+    return mylist[0][0] == mylist[1][0]
+
+print(myfunc2_solution('Levelheaded Llama'))
+print(myfunc2_solution('Crazy Kangaroo'))
 
 #MAKES TWENTY:
 #Given two integers, return True if the sum of the integers is 20 or
 #if one of the integers is 20. If not, return False
 
 def myfunc3(a,b):
-    if a + b == 20 or a==20 or b==20:
-        return True
-    else:
-        return False
+    return a + b == 20 or a==20 or b==20
 
 print(myfunc3(20,10))
 print(myfunc3(12,8))
 print(myfunc3(2,3))
 
 # LEVEL 1
-
 #OLD MACDONALD:
 #Write a function that capitalizes the first and fourth letters of a name
 
