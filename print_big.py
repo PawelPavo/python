1e10363c6734e542340a4d5b56066d65d31c2606
PawelPavo <Pawelebllc@gmail.com>
2022-05-30 11:29:25 -0400

    problem solved

--- print_big.py
+++ print_big.py
@@ -1,5 +1,5 @@
 
-#ust for fun, not a real problem :)
+#just for fun, not a real problem :)
 #PRINT BIG: Write a function that takes in a single letter, and returns a 5x5 representation of that letter
 #print_big('a')
 
@@ -12,5 +12,22 @@
 #For purposes of this exercise, it's ok if your dictionary stops at "E".
 
 
+
 def print_big(letter):
-    pass
+    patterns={1:'  *  ', 2:' * * ',3:' *** ', 4:'*   *',5:'*****',6:'*',7:'* * *', 8:'** **'}
+    text={'A':[1,2,5,4,4],'B':[5,4,5,4,5],'P':[5,4,5,6,6], 'W':[4,4,7,8,4],'E':[5,6,5,6,5], 'L':[6,6,6,6,5]}
+    for pattern in text[letter.upper()]:
+        print(patterns[pattern])
+
+
+print()
+print_big('p')
+print()
+print_big('a')
+print()
+print_big('w')
+print()
+print_big('e')
+print()
+print_big('l')
+print()
