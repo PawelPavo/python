c048e0123fd54d6a9ce754e992009047f647556c
PawelPavo <Pawelebllc@gmail.com>
2022-05-30 10:51:29 -0400

    first commint - print_big

---
+++ print_big.py
@@ -0,0 +1,16 @@
+
+#ust for fun, not a real problem :)
+#PRINT BIG: Write a function that takes in a single letter, and returns a 5x5 representation of that letter
+#print_big('a')
+
+#out:   *  
+#      * *
+#     *****
+#     *   *
+#     *   *
+#HINT: Consider making a dictionary of possible patterns, and mapping the alphabet to specific 5-line combinations of patterns.
+#For purposes of this exercise, it's ok if your dictionary stops at "E".
+
+
+def print_big(letter):
+    pass
