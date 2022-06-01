e846b3df8ff864c4cd12289f8ecf799809603cda
PawelPavo <59782585+PawelPavo@users.noreply.github.com>
2022-06-01 16:31:30 -0400

    Add files via upload

--- methods_functions_homework.py
+++ methods_functions_homework.py
@@ -172,5 +172,37 @@ print(palindrome_2('abcba'))
 #Write a Python function to check whether a string is pangram or not.
 #(Assume the string passed in does not have any punctuation)
 
+#Note : Pangrams are words or sentences containing every letter of the alphabet at least once.
+#For example : "The quick brown fox jumps over the lazy dog"
+
+def ispangram(str1, alpha=string.ascii_lowercase):
+    str2=str1.lower().replace(' ','')
+    return set(str2)==set(alpha)
+
+
+print(ispangram("The quick brown fox jumps over the lazy dog"))
+
+#solution
+
+import string
+
+def ispangram_2(str1, alphabet=string.ascii_lowercase): 
+    # Create a set of the alphabet
+    alphaset = set(alphabet)  
+    
+    # Remove spaces from str1
+    str1 = str1.replace(" ",'')
+    
+    # Lowercase all strings in the passed in string
+    # Recall we assume no punctuation 
+    str1 = str1.lower()
+    
+    # Grab all unique letters in the string as a set
+    str1 = set(str1)
+    
+    # Now check that the alpahbet set is same as string set
+    return str1 == alphaset
+ispangram_2("The quick brown fox jumps over the lazy dog")
+
 
 
