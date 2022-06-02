133037e8624b1cb4914f8c173c2cef38c856772e
PawelPavo <Pawelebllc@gmail.com>
2022-06-02 19:29:37 -0400

    Step 3

--- TicTacToe/tic_tac_toe.py
+++ TicTacToe/tic_tac_toe.py
@@ -1,1 +1,25 @@
 #TicTacToe
+
+from re import X
+from secrets import choice
+
+
+board=['#','X','O','X','O','X','O','X','O','X']
+
+def display_board(board):
+    print(board[0],board[1],board[2])
+    print(board[3],board[4],board[5])
+    print(board[6],board[7],board[8])
+
+#display_board(board)
+
+def player_input():
+    choice = input('Please enter X or O: ')
+    while choice.upper() not in ['X','O']:
+        print(f'{choice} is not a valid choice!')
+        choice = input('Please enter X or O: ')
+
+#player_input()
+
+def place_marker(board, marker, position):
+    pass
