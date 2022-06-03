9e0f2e9856f9a4d156023d4bb3d69213847f7812
PawelPavo <59782585+PawelPavo@users.noreply.github.com>
2022-06-03 10:38:11 -0400

    Add files via upload

--- TicTacToe/tic_tac_toe.py
+++ TicTacToe/tic_tac_toe.py
@@ -1,10 +1,8 @@
 #TicTacToe
-
-from re import X
-from secrets import choice
+import random
 
 
-board=['#','X','O','X','O','X','O','X','O','X']
+board=['1','2','3','4','5','6','7','8','9']
 
 def display_board(board):
     print(board[0],board[1],board[2])
@@ -14,12 +12,83 @@ def display_board(board):
 #display_board(board)
 
 def player_input():
-    choice = input('Please enter X or O: ')
-    while choice.upper() not in ['X','O']:
-        print(f'{choice} is not a valid choice!')
-        choice = input('Please enter X or O: ')
+    marker='WRONG'
+    while marker.upper() not in ['X','O']:
+        marker = input('Please enter X or O: ')
+        if marker.upper() not in ['X','Y']:
+            print(f'Sorry, {marker} is not a valide choise!')
+    return marker.upper()
 
-#player_input()
+#marker=player_input()
 
-def place_marker(board, marker, position):
-    pass
+def position_choice():
+    
+    # VARS
+    position='WRONG'
+    acceptable_range = range(0,10)
+    within_range = False
+    
+    # TWO CONDITIONS TO CHECK - DIGIT or WITHIN_RANGE == False
+    while position.isdigit()== False or within_range ==False:
+        position = input('Please enter the position for your marker: ')
+
+        # Digit Check
+        if position.isdigit()==False:
+            print(f'Sorry {position} is not a digit! Try again.')
+
+        # range Check
+        if position.isdigit()==True:
+            if int(position) in acceptable_range:
+                within_range=True
+            else:
+                within_range=False
+                print('Please enter a valid position on the board!')
+        return int(position)
+     
+#position=position_choice()
+
+def place_marker(board,marker,position):
+    board[position-1] = marker
+    display_board(board)
+
+#place_marker(board,marker,position)
+
+
+def win_check(board,marker):
+    winner=[]
+    win_combos={'combo1':[0,1,2],
+                'combo2':[3,4,5],
+                'combo3':[6,7,8],
+                'combo4':[0,3,6],
+                'combo5':[1,4,7],
+                'combo6':[2,5,8],
+                'combo7':[6,4,2],
+                'combo8':[0,4,8]}
+    for index,char in enumerate(board):
+        if char == marker:
+            winner.append(index)
+            for win in win_combos.values():
+                if winner == win:
+                    print(f'{marker} is the winner!')
+    
+
+win_check(board,'X')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
