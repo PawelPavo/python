6f84c3fef2f7d27b6c699e84899e7fdd40d423b5
PawelPavo <59782585+PawelPavo@users.noreply.github.com>
2022-06-06 14:22:28 -0400

    Add files via upload

--- TicTacToe/tic_tac_toe.py
+++ TicTacToe/tic_tac_toe.py
@@ -1,25 +1,51 @@
-#TicTacToe
-import random
+from random import choice
+
+print('Welcome to Tic Tac Toe?')
+board=[' ',' ',' ',' ',' ',' ',' ',' ',' ']
+turn=0
+play = True
+
+def play():
+    play_question=position = input('Do you want to play again? Type Y or N: ')
+    if play_question=='Y':
+        play=True
+    else:
+        print('Thanks, see you later!')
+        
+
+def choose_first():
+    player_1= choice([1,2])
+    print("Player {} will go first.".format(player_1))
+    return player_1
+
+player_1=choose_first()
+
+def choose_marker(player_1):
+        marker='WRONG'
+        markers=['X','O']
+        while marker.upper() not in markers:
+            marker = input('Please select X or O: ')
+            if marker.upper() not in markers:
+                print(f'Sorry, {marker} is not a valide choice!')
+        if marker.upper() == 'X':
+            player_1 = 'X'
+            player_2 = 'O'
+        else:
+            player_1 = 'O'
+            player_2 = 'X'
+        return player_1,player_2
 
 
-board=['1','2','3','4','5','6','7','8','9']
+marker=list(choose_marker(player_1))
 
 def display_board(board):
-    print(board[0],board[1],board[2])
-    print(board[3],board[4],board[5])
-    print(board[6],board[7],board[8])
+    print(board[0]+'|'+ board[1]+'|'+ board[2])
+    print('-----')
+    print(board[3]+'|'+board[4]+'|'+board[5])
+    print('-----')
+    print(board[6]+'|'+board[7]+'|'+board[8])
 
-#display_board(board)
-
-def player_input():
-    marker='WRONG'
-    while marker.upper() not in ['X','O']:
-        marker = input('Please enter X or O: ')
-        if marker.upper() not in ['X','Y']:
-            print(f'Sorry, {marker} is not a valide choise!')
-    return marker.upper()
-
-#marker=player_input()
+display_board(board)
 
 def position_choice():
     
@@ -29,13 +55,13 @@ def position_choice():
     within_range = False
     
     # TWO CONDITIONS TO CHECK - DIGIT or WITHIN_RANGE == False
-    while position.isdigit()== False or within_range ==False:
+    while position.isdigit()== False or within_range == False:
         position = input('Please enter the position for your marker: ')
-
         # Digit Check
         if position.isdigit()==False:
             print(f'Sorry {position} is not a digit! Try again.')
-
+            continue
+        
         # range Check
         if position.isdigit()==True:
             if int(position) in acceptable_range:
@@ -43,52 +69,60 @@ def position_choice():
             else:
                 within_range=False
                 print('Please enter a valid position on the board!')
+                continue      
         return int(position)
-     
-#position=position_choice()
 
 def place_marker(board,marker,position):
-    board[position-1] = marker
+    global turn
+    if turn%2==0:
+        board[position] = marker[0]
+    else:
+        board[position] = marker[1]
     display_board(board)
-
-#place_marker(board,marker,position)
-
+    turn+=1
+  
+def space_check(board, position):
+    if board[position]=='X' or board[position]=='O':
+        return True
+        
 
 def win_check(board,marker):
-    winner=[]
-    win_combos={'combo1':[0,1,2],
-                'combo2':[3,4,5],
-                'combo3':[6,7,8],
-                'combo4':[0,3,6],
-                'combo5':[1,4,7],
-                'combo6':[2,5,8],
-                'combo7':[6,4,2],
-                'combo8':[0,4,8]}
-    for index,char in enumerate(board):
-        if char == marker:
-            winner.append(index)
-            for win in win_combos.values():
-                if winner == win:
-                    print(f'{marker} is the winner!')
+    if board[0]=='X' and board[1]=='X' and board[2]=='X' or\
+       board[3]=='X' and board[4]=='X' and board[5]=='X' or\
+       board[6]=='X' and board[7]=='X' and board[8]=='X' or\
+       board[0]=='X' and board[3]=='X' and board[6]=='X' or\
+       board[1]=='X' and board[3]=='4' and board[7]=='X' or\
+       board[2]=='X' and board[5]=='4' and board[8]=='X' or\
+       board[0]=='X' and board[4]=='4' and board[8]=='X' or\
+       board[2]=='X' and board[4]=='4' and board[6]=='X':
+           print('X is the winner')
+           play()
+    if board[0]=='O' and board[1]=='O' and board[2]=='O' or\
+       board[3]=='O' and board[4]=='O' and board[5]=='O' or\
+       board[6]=='O' and board[7]=='O' and board[8]=='O' or\
+       board[0]=='O' and board[3]=='O' and board[6]=='O' or\
+       board[1]=='O' and board[3]=='O' and board[7]=='O' or\
+       board[2]=='O' and board[5]=='O' and board[8]=='O' or\
+       board[0]=='O' and board[4]=='O' and board[8]=='O' or\
+       board[2]=='O' and board[4]=='O' and board[6]=='O':
+           print('O is the winner')
+           play()
+
+def full_board_check(board):
+    for space in board:
+        if space == ' ':
+            return False
+    return True
     
 
-win_check(board,'X')
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+while not full_board_check(board):
+    position=position_choice()
+    if space_check(board,position):
+        print('Sorry, this position is taken.')
+    else:
+        place_marker(board,marker,position)
+    if win_check(board,marker)== True or turn==9:
+        if turn ==9:
+            print('DRAW!')
+            play()
+        break
