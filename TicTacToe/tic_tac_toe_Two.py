831e16d3c892b2a7f4efeca1cef7ac85dd9ecfe6
PawelPavo <59782585+PawelPavo@users.noreply.github.com>
2022-06-06 14:52:31 -0400

    Add files via upload

--- TicTacToe/tic_tac_toe_Two.py
+++ TicTacToe/tic_tac_toe_Two.py
@@ -3,22 +3,11 @@ from random import choice
 print('Welcome to Tic Tac Toe?')
 board=[' ',' ',' ',' ',' ',' ',' ',' ',' ']
 turn=0
-play = True
-
-def play():
-    play_question=position = input('Do you want to play again? Type Y or N: ')
-    if play_question=='Y':
-        play=True
-    else:
-        print('Thanks, see you later!')
-        break
 
 def choose_first():
     player_1= choice([1,2])
     print("Player {} will go first.".format(player_1))
     return player_1
-
-player_1=choose_first()
 
 def choose_marker(player_1):
         marker='WRONG'
@@ -35,9 +24,6 @@ def choose_marker(player_1):
             player_2 = 'X'
         return player_1,player_2
 
-
-marker=list(choose_marker(player_1))
-
 def display_board(board):
     print(board[0]+'|'+ board[1]+'|'+ board[2])
     print('-----')
@@ -45,10 +31,7 @@ def display_board(board):
     print('-----')
     print(board[6]+'|'+board[7]+'|'+board[8])
 
-display_board(board)
-
 def position_choice():
-    
     # VARS
     position='WRONG'
     acceptable_range = range(0,10)
@@ -80,49 +63,72 @@ def place_marker(board,marker,position):
         board[position] = marker[1]
     display_board(board)
     turn+=1
-  
+
 def space_check(board, position):
     if board[position]=='X' or board[position]=='O':
         return True
         
-
 def win_check(board,marker):
     if board[0]=='X' and board[1]=='X' and board[2]=='X' or\
        board[3]=='X' and board[4]=='X' and board[5]=='X' or\
        board[6]=='X' and board[7]=='X' and board[8]=='X' or\
        board[0]=='X' and board[3]=='X' and board[6]=='X' or\
-       board[1]=='X' and board[3]=='4' and board[7]=='X' or\
+       board[1]=='X' and board[4]=='4' and board[7]=='X' or\
        board[2]=='X' and board[5]=='4' and board[8]=='X' or\
        board[0]=='X' and board[4]=='4' and board[8]=='X' or\
        board[2]=='X' and board[4]=='4' and board[6]=='X':
            print('X is the winner')
            play()
+           return True
     if board[0]=='O' and board[1]=='O' and board[2]=='O' or\
        board[3]=='O' and board[4]=='O' and board[5]=='O' or\
        board[6]=='O' and board[7]=='O' and board[8]=='O' or\
        board[0]=='O' and board[3]=='O' and board[6]=='O' or\
-       board[1]=='O' and board[3]=='O' and board[7]=='O' or\
+       board[1]=='O' and board[4]=='O' and board[7]=='O' or\
        board[2]=='O' and board[5]=='O' and board[8]=='O' or\
        board[0]=='O' and board[4]=='O' and board[8]=='O' or\
        board[2]=='O' and board[4]=='O' and board[6]=='O':
            print('O is the winner')
            play()
+           return True
 
 def full_board_check(board):
     for space in board:
         if space == ' ':
             return False
     return True
+
+def play():
+    global turn, board
+    play_question=position = input('Do you want to play again? Type Y or N: ')
+    if play_question=='Y':
+        board=[' ',' ',' ',' ',' ',' ',' ',' ',' ']
+        turn=0
+        play_tic_tac_toe()
+    else:
+        print('Thanks, see you later!')
+    return
+
+def play_tic_tac_toe():
+    player_1=choose_first()
+    marker=list(choose_marker(player_1))
+    display_board(board)
+    while not full_board_check(board):
+        position=position_choice()
+        if space_check(board,position):
+            print('Sorry, this position is taken.')
+        else:
+            place_marker(board,marker,position)
+        if win_check(board,marker)== True or turn==9:
+            if turn ==9:
+                print('DRAW!')
+                play()
+            break
+
+play_tic_tac_toe()
+
     
 
-while not full_board_check(board):
-    position=position_choice()
-    if space_check(board,position):
-        print('Sorry, this position is taken.')
-    else:
-        place_marker(board,marker,position)
-    if win_check(board,marker)== True or turn==9:
-        if turn ==9:
-            print('DRAW!')
-            play()
-        break
+
+
+
