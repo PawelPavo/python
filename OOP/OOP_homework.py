6d9fe553ce441dc2d386af35b7dd1a947073699f
PawelPavo <59782585+PawelPavo@users.noreply.github.com>
2022-06-09 14:38:55 -0400

    Add tuple unpacking solution

--- OOP/OOP_homework.py
+++ OOP/OOP_homework.py
@@ -10,6 +10,12 @@ class Line:
     
     def distance(self):
         return ((self.coor2[0]-self.coor1[0])**2 +(self.coor2[1]-self.coor1[1])**2)**0.5
+
+    # touple unpacking
+    def distance_tuple_unpacking(self):
+        x1,y1=self.coor1
+        x2,y2=self.coor2
+        return ((x2-x1)**2 +(y2-y1)**2)**0.5
     
     def slope(self):
         return (self.coor2[1]-self.coor1[1])/(self.coor2[0]-self.coor1[0])
@@ -22,6 +28,8 @@ coordinate2 = (8,10)
 
 li = Line(coordinate1,coordinate2)
 print(li.distance())
+
+print(li.distance_tuple_unpacking())
 
 print(li.slope())
 
