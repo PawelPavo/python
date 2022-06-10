9e6b865fe7f550a06c82eb2c83f5ed29e1844f37
PawelPavo <59782585+PawelPavo@users.noreply.github.com>
2022-06-10 08:49:08 -0400

    Add files via upload

---
+++ OOP/OOP_challenge.py
@@ -0,0 +1,68 @@
+# For this challenge, create a bank account class that has two attributes:
+
+# - owner
+# - balance
+
+# and two methods:
+
+# - deposit
+# - withdraw
+
+# As an added requirement, withdrawals may not exceed the available balance.
+# Instantiate your class, make several deposits and withdrawals, and test to make sure the account can't be overdrawn.
+
+
+
+class Account():
+    def __init__(self,owner,balance):
+        self.owner=owner
+        self.balance=balance
+
+    def owner(self):
+        return self.owner
+
+    def balance(self):
+        return self.balance
+
+    def balance(self):
+        print(self.balance)
+    
+    def deposit(self,amount):
+        self.balance = self.balance+amount
+        print(f'Your new balance is ${acct1.balance}')
+        return self.balance
+    
+    def withdraw(self, amount):
+        if self.balance < amount:
+            print('Fundes unavailable!')
+        else:
+            self.balance = self.balance-amount
+            print(f'Your new balance is ${acct1.balance}')
+        return self.balance
+
+    def __str__(self):
+        return f'Account owner: {self.owner} \nAccount balance: ${self.balance}'
+        
+
+
+# 1. Instantiate the class
+acct1 = Account('Jose',100)
+
+# 2. Print the object
+print(acct1)
+
+# 3. Show the account owner attribute
+print(acct1.owner)
+
+# 4. Show the account balance attribute
+print(acct1.balance)
+
+# 5. Make a series of deposits and withdrawals
+acct1.deposit(50)
+acct1.deposit(50)
+acct1.deposit(50)
+print(acct1)
+
+# 6. Make a withdrawal that exceeds the available balance
+acct1.withdraw(251)
+
