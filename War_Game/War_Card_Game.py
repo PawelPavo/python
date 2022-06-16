eec27bc5696ab9e0519d239b4ca03c3e10c18ed1
PawelPavo <59782585+PawelPavo@users.noreply.github.com>
2022-06-16 14:12:49 -0400

    Add files via upload

--- War_Game/War_Card_Game.py
+++ War_Game/War_Card_Game.py
@@ -15,6 +15,12 @@ class Card:
     def __str__(self):
         return self.rank + ' of ' + self.suit
 
+# TESTING Card CLASS and value compare
+'''
+two_hearts= Card("Hearts","Two")
+three_clubs=Card("Clubs", "Three")
+print(three_clubs.value < two_hearts.value)
+'''
 
 # DECK CLASS
 # Created a deck with an object of 52 cards
@@ -36,7 +42,6 @@ class Deck:
     def deal_one(self):
         return self.all_cards.pop()
 
-
 # TESTING shuffle_deck METHOD of DECK CLASS
 '''
 new_deck=Deck()
@@ -47,7 +52,6 @@ print(new_deck.all_cards[-1])
 '''
 
 # TESTING deal_one METHOD of DECK CLASS
-
 '''
 new_deck=Deck()
 new_deck.shuffle_deck()
@@ -55,3 +59,51 @@ myCard=new_deck.deal_one()
 print(myCard)
 print(len(new_deck.all_cards))
 '''
+
+# PLAYER CLASS
+# Creating a class for Players
+
+class Player:
+    def __init__(self,name):
+        self.name=name
+        self.all_cards=[]
+
+    #removes the card from the top of the players deck - from the start of the list
+    def remove_one(self):
+        return self.all_cards.pop(0) 
+
+     # adds cards to the bottom of the players deck - to the end of the list
+    def add_cards(self, new_cards):
+        # 'if' statement that compares the TYPE of the args 'if list'
+        # if it is a list, that means there are more than 1 cards to add to the deck
+
+        if type(new_cards)==type([]):
+            # adding a LIST of Card objects - list of cards
+            self.all_cards.extend(new_cards) # use of 'extend()'
+        else:
+            # adding a SINGLE  Card object
+            self.all_cards.append(new_cards) # use of 'append()'
+
+    def __str__(self):
+        return f'Player {self.name} has {len(self.all_cards)} cards.'
+
+# TESTING Player CLASS and METHODS
+'''
+new_deck=Deck() # Create a new deck
+new_player=Player("Pawel")  # Create a player   
+myCard=new_deck.deal_one() # deal one card
+print(myCard) # check what card was delt
+new_player.add_cards(myCard) # player 1 gets the delt card
+print(new_player) # check if the player has the card
+print(new_player.all_cards[0]) # check if its the card matched
+
+# checking to see if list of cards adds correctly (same card is added for example)
+new_player.add_cards([myCard,myCard,myCard]) 
+print(new_player)
+
+# checking remove method
+new_player.remove_one()
+print(new_player)
+'''
+
+
