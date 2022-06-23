f5c7c2f12d06f291c6760ba6b5377d5f1b8af6be
PawelPavo <59782585+PawelPavo@users.noreply.github.com>
2022-06-23 10:59:59 -0400

    Created Deck and Card Class

--- Blackjack/Blacjack.py
+++ Blackjack/Blacjack.py
@@ -1,1 +1,36 @@
 # BLACKJACK
+import random
+
+
+# Creating a Deck of 52 Cards
+suits = ('Hearts', 'Diamonds', 'Spades', 'Clubs')
+ranks = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King', 'Ace')
+values = {'Two':2, 'Three':3, 'Four':4, 'Five':5, 'Six':6, 'Seven':7, 'Eight':8, 'Nine':9, 'Ten':10, 'Jack':10,
+         'Queen':10, 'King':10, 'Ace':11}
+
+playing=True
+
+
+class Card:
+    def __init__(self,suit,rank):
+        self.suit=suit
+        self.rank=rank
+        self.value=values[rank]
+    def __str__(self):
+        return self.rank + " of " + self.suit
+
+
+
+class Deck:
+    def __init__(self):
+        self.all_cards=[]
+        for suit in suits:
+            for rank in ranks:
+                created_card=Card(suit,rank)
+                self.all_cards.append(created_card)
+    def shuffle(self):
+        random.shuffle(self.all_cards)
+
+    def deal(self):
+        return self.all_cards.pop()
+
