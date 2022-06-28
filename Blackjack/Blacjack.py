a84a55d90b0b707f5c9eaf6b31052a218388cd9f
PawelPavo <59782585+PawelPavo@users.noreply.github.com>
2022-06-28 14:02:32 -0400

    Setting up functions of game play

--- Blackjack/Blacjack.py
+++ Blackjack/Blacjack.py
@@ -85,3 +85,99 @@ class Chips:
 
     def lose_bet(self):
         self.total+=self.bet
+
+
+
+# Asking player how many chips they want to bet and checking if they have enough
+def take_bet(chips):
+    while True:
+        try:
+            chips.bet=int(input('How many chips would you like to bet? '))
+        except:
+            print('Sorry please provide an integer.')
+        else:
+            if chips.bet > chips.total:
+                print('Sorry, you do not have enough chiops! You have: {} chips left.'.format(chips.total))
+            else:
+                break
+
+# function to hit
+def hit(deck,hand):
+
+    # adding card from the hand and adjusting for Ace
+    single_card=deck.deal()
+    hand.add_card(single_card)
+    hand.adjust_for_ace()
+
+
+# function to see if player wants to hit or stand
+def hit_or_stand(deck,hand):
+    global playing # controls a while loop
+    while True:
+        x = input('Hit or Stanb? Enter h or s ')
+        if x[0].lower() == 'h':  # x[0] is to check if someone may write "hit" or "hh" for example
+            hit(deck,hand)
+        elif x[0].lower() == 's':
+            print("Player Stands Dealer's Turn")
+            playing=False
+        else:
+            print('Sorry, I did not understand that, please eneter h or s only! ')
+            continue
+        break
+
+# Functions to display cards
+
+def show_some(player, dealer):
+    # Show only ONE of dealers hand/cards
+    print("\n Dealer's Hand: ")
+    print("First card hidden!")
+    print(dealer.cards[1])
+    
+    # Show all (2 cards) of the players hand/cards
+    print("\n Player's Hand: ")
+    for card in player.cards:
+        print(card)
+
+
+def show_all(player, dealer):
+    # Show all the dealers cards
+    print("\n Dealer's Hand: ")
+    for card in dealer.cards:
+        print(card)
+
+    # One line to for for loop
+    '''
+    print(f"Dealer's hand: ",*dealer.cards,sep='\n')
+    '''
+    
+        
+    # calculare and display value ex. (J+K = 20)
+    print("Value of Dealer's Hand is: {}".fomat(dealer.value))
+
+    # Show all the players cards
+    print("\n Player's Hand: ")
+    for card in player.cards:
+        print(card)
+    
+    print("Value of Players's Hand is: {}".fomat(player.value))
+
+# End of game scenarios
+
+def player_busts(player,dealer,chips):
+    print('BUST PLAYER')
+    chips.lose_bet()
+
+def player_wins(player,dealer,chips):
+    print('PLAYER WINS')
+    chips.win_bet()
+
+def dealer_busts(player,dealer,chips):
+    print('PLAYER WINS! DEALER BUSTED!')
+    chips.win_bet()
+
+def dealer_wins(player,dealer,chips):
+    print('DEALER WINS!')
+    chips.lose_bet()
+
+def push(player,dealer):
+    print('Dealer and Player tie! PUSH')
