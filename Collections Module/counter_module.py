f29a4289de7d0388fc0dba74cc897e244e64d899
PawelPavo <59782585+PawelPavo@users.noreply.github.com>
2022-07-01 09:56:46 -0400

    use of Counter()

---
+++ Collections Module/counter_module.py
@@ -0,0 +1,16 @@
+from collections import Counter
+
+mylist=[1,1,1,1,1,2,2,2,2,3,3,3,3,3,3]
+
+print(Counter(mylist))
+
+print(Counter('dsdfsfdPawelfdfsssskkkeepas'))
+
+sentence="How many times does each word show up in this sentence with a word"
+print(Counter(sentence.split()))
+
+
+letters='aaabbbbccccdddeee'
+
+c=Counter(letters)
+print(c.most_common(2))
