c095e242bfdea33f65c030b77a57ce78ce692e5b
PawelPavo <59782585+PawelPavo@users.noreply.github.com>
2022-07-13 08:51:21 -0400

    Add files via upload

--- Timing the Code/timing_the_code_import_timeit.py
+++ Timing the Code/timing_the_code_import_timeit.py
@@ -1,5 +1,6 @@
 import timeit
 
+#Running both functions 100,000 times to see which is faster
 
 # FUNCTION 1
 stmt_1= '''
@@ -11,12 +12,11 @@ def func_one(n):
     return [str(num) for num in range(n)]
 '''
 
+
 print(timeit.timeit(stmt_1,setup_1,number=100000))
 
 
 # FUNCTION 2
-
-
 stmt_2='''
 func_two(100)
 '''
