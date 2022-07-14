532380203ed3f896d4bff95537c068f6d9d8c0d7
PawelPavo <59782585+PawelPavo@users.noreply.github.com>
2022-07-14 10:57:31 -0400

    Add files via upload

--- Exercise Puzzle/exercise_puzzle.py
+++ Exercise Puzzle/exercise_puzzle.py
@@ -6,6 +6,10 @@ import re
 shutil.unpack_archive('unzip_me_for_instructions.zip','instructions_unzipped','zip')
 file_path="C:/Users/pjaskolski/Desktop/Python/Exercise Puzzle/instructions_unzipped/extracted_content"
 
+inst=open('C:/Users/pjaskolski/Desktop/Python/Exercise Puzzle/instructions_unzipped/extracted_content/Instructions.txt')
+print(inst.read())
+
+
 phone_pattern=re.compile(r'(\d{3})-(\d{3})-(\d{4})')
 
 for folder, sub_folders, files in os.walk(file_path):
