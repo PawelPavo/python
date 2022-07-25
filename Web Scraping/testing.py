245b4ca5a78d7b96e25c885b93a8467ef7c8eaea
PawelPavo <Pawelebllc@gmail.com>
2022-07-25 15:53:19 -0400

    Making API call to blogs

--- Web Scraping/testing.py
+++ Web Scraping/testing.py
@@ -1,17 +1,22 @@
 import requests
 import bs4
 
-page_num=0
-valid_page=True
+# page_num=0
+# valid_page=True
 
-while valid_page:
-    base_url=("https://www.xolobooks.com/bookstore?page={}")
-    scrape_url=base_url.format(page_num)
-    res=requests.get(scrape_url)
-    soup=bs4.BeautifulSoup(res.text,'lxml')
-    button=soup.select('.txtqbB')
-    if button == []:
-        print('NO MORE PAGES')
-        break
-    page_num+=1
-print(page_num)
+# while valid_page:
+#     base_url=("https://www.xolobooks.com/bookstore?page={}")
+#     scrape_url=base_url.format(page_num)
+#     res=requests.get(scrape_url)
+#     soup=bs4.BeautifulSoup(res.text,'lxml')
+#     button=soup.select('.txtqbB')
+#     if button == []:
+#         print('NO MORE PAGES')
+#         break
+#     page_num+=1
+# print(page_num)
+
+res=requests.get('https://mighty-fortress-59305.herokuapp.com/api/blogs')
+blogs=(res.json())
+
+print(blogs[0])
